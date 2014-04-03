7b8684e447c62c10935c7478d6c3415bfe350e37
John Murray <me@johnmurray.io>
2014-04-03 17:21:33 -0400

    Config Loader Spec - Placeholder
    Just a reminder for myself that I need to thoroughly test this feature

---
+++ src/test/scala/ConfigLoaderSpec.scala
@@ -0,0 +1,18 @@
+import org.specs2.mutable.Specification
+import akka.testkit._
+
+/**
+ * Author: John Murray <jmurray@appnexus.com>
+ * Date:   4/3/14
+ *
+ * Specification for the config loader actor
+ */
+class ConfigLoaderSpec extends Specification {
+
+   "config loader actor" should {
+      "placeholder" in {
+         1 must_== 1
+      }
+   }
+
+}
