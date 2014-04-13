113e106aee4f9daad4aff3381b899011b759b557
John Murray <me@johnmurray.io>
2014-04-13 15:16:48 -0400

    Formatted project/Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -3,48 +3,51 @@ import Keys._
 
 
 object Resolvers {
-  val sprayResolver = "spray repo" at "http://repo.spray.io"
+
+   val sprayResolver = "spray repo" at "http://repo.spray.io"
 }
 
 object Dependencies {
-  val akkaV = "2.3.0"
-  val sprayV = "1.3.1"
 
-  val appDependencies = Seq(
-    "com.typesafe.akka"   %%  "akka-actor"   % akkaV,
-    "com.typesafe.akka"   %%  "akka-testkit" % akkaV    % "test",
-    "io.spray"             %  "spray-can"    % sprayV,
-    "io.spray"            %%  "spray-json"   % "1.2.5",
-    "org.specs2"          %%  "specs2"       % "2.3.10" % "test"
-  )
+   val akkaV = "2.3.0"
+   val sprayV = "1.3.1"
+
+   val appDependencies = Seq(
+      "com.typesafe.akka" %% "akka-actor"   % akkaV,
+      "com.typesafe.akka" %% "akka-testkit" % akkaV     % "test",
+      "io.spray"          %  "spray-can"    % sprayV,
+      "io.spray"          %% "spray-json"   % "1.2.5",
+      "org.specs2"        %% "specs2"       % "2.3.10"  % "test"
+   )
 }
 
 object BuildSettings {
-  val buildOrganization = "johnmurray.io"
-  val appName = "router"
-  val buildVersion = "0.0.1-SNAPSHOT"
-  val buildScalaVersion = "2.10.3"
-  val buildScalaOptions = Seq("-unchecked", "-deprecation", "-encoding", "utf8")
 
-  import Resolvers._
+   val buildOrganization = "johnmurray.io"
+   val appName = "router"
+   val buildVersion = "0.0.1-SNAPSHOT"
+   val buildScalaVersion = "2.10.3"
+   val buildScalaOptions = Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+
+   import Resolvers._
    import Dependencies._
 
-  val buildSettings = Defaults.defaultSettings ++ Seq(
-    organization        := buildOrganization,
-    version             := buildVersion,
-    scalaVersion        := buildScalaVersion,
-    resolvers           += sprayResolver,
-    libraryDependencies := appDependencies,
-    scalacOptions       := buildScalaOptions
-  )
+   val buildSettings = Defaults.defaultSettings ++ Seq(
+      organization        := buildOrganization,
+      version             := buildVersion,
+      scalaVersion        := buildScalaVersion,
+      resolvers           += sprayResolver,
+      libraryDependencies := appDependencies,
+      scalacOptions       := buildScalaOptions
+   )
 }
 
 object ApplicationBuild extends Build {
 
-  import BuildSettings._
+   import BuildSettings._
 
-  lazy val main = Project(
-    appName,
-    file("."),
-    settings = buildSettings)
+   lazy val main = Project(
+      appName,
+      file("."),
+      settings = buildSettings)
 }
